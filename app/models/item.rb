a3f70d46ddf41b5c267acb84eb8196866cc01e4b
nekoyarow <akiko.i.b511@gmail.com>
2022-08-18 11:39:06 +0900

    ActiveHashとitemのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :date
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
