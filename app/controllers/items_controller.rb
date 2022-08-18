901d1b403e624f4ad2314c9459838dadb1aee8d0
nekoyarow <akiko.i.b511@gmail.com>
2022-08-18 21:58:38 +0900

    Showの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,11 +18,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :cost_id, :area_id, :shipdate_id,
-                                 :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :cost_id, :area_id, :shipdate_id,:price).merge(user_id: current_user.id)
   end
 
   def move_to_login
