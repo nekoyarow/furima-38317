8e82c1256666be9475e4a3f15c1df6316df74b32
nekoyarow <akiko.i.b511@gmail.com>
2022-08-19 20:55:44 +0900

    before action設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_login, only: [:new]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order(created_at: 'DESC')
@@ -23,7 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    
+    @item = Item.find(params[:id])
   end
 
   def update
@@ -38,6 +39,15 @@ class ItemsController < ApplicationController
   end
 
   def move_to_login
-    redirect_to new_user_session_path unless user_signed_in?
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == item.user_id
+      redirect_to root_path
+    end
   end
 end
