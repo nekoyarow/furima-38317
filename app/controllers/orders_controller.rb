efe87ef7d3e00ac2b04eec4e558c870efe6b29d5
nekoyarow <akiko.i.b511@gmail.com>
2022-08-22 18:54:32 +0900

    ページ遷移の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_root_path, only: :index
+
   def index
     
+  end
+
+  private
+  def move_to_index
+    if current_user.id == @item.user_id
+      redirect_to root_path 
+    end
   end
 end
